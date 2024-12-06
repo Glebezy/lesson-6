ef7362aeff4f25d7a121f8ca5725fdf5fd7feac7
Глеб Троц <gntrots@mts.ru>
2024-12-06 19:02:32 +0500

    fixed condition

--- test_functions.py
+++ test_functions.py
@@ -28,7 +28,9 @@ def test_dark_theme_by_time_and_user_choice():
     # TODO переключите темную тему в зависимости от времени суток,
     #  но учтите что темная тема может быть включена вручную
 
-    if dark_theme_enabled_by_user or current_time.hour >= 22 or current_time.hour < 6:
+    if dark_theme_enabled_by_user is not None:
+        is_dark_theme = dark_theme_enabled_by_user
+    elif current_time.hour >= 22 or current_time.hour < 6:
         is_dark_theme = True
     else:
         is_dark_theme = False
